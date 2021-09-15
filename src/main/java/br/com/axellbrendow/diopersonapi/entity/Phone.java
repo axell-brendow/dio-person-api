fb59825da4256944fdcd7937620b89b33ccb0586
axell-brendow <breno.axel@gmail.com>
2021-09-15 10:42:59 -0300

    Auto generate Phone id

--- src/main/java/br/com/axellbrendow/diopersonapi/entity/Phone.java
+++ src/main/java/br/com/axellbrendow/diopersonapi/entity/Phone.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import br.com.axellbrendow.diopersonapi.enums.PhoneType;
@@ -19,6 +21,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Phone {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
