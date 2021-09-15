95bb963827712471daa5a1ca88c6cbb37ee891fd
axell-brendow <breno.axel@gmail.com>
2021-09-15 09:01:02 -0300

    Initial upload

---
+++ src/main/java/br/com/axellbrendow/diopersonapi/DioPersonApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diopersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPersonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/axellbrendow/diopersonapi/DioPersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.diopersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
